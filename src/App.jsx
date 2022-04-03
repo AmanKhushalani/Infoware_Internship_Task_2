ab7d27451dd5c0de5332b6d8694d2cdf77dc3984
AmanKhushalani <khushalaniaman1@gmail.com>
2022-04-03 12:59:59 +0530

    Added theme object !

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Form from "./Components/Form";
 import Header from "./Components/Header";
 import MyTimeline from "./Components/Timeline";
 import './Styles/index.css'
-import { createTheme,ThemeProvider } from '@mui/material/styles';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
 
 
 
@@ -22,16 +22,16 @@ const theme = createTheme({
         fontWeightMedium: 300,
         fontWeightBold: 400,
     },
-    autocomplete : {
-        color : "rgb(144, 202, 249) !important"
+    autocomplete: {
+        color: "rgb(144, 202, 249) !important"
     },
     "& #combo-box-demo": {
         //or could be targeted through a class
-        innerWidth : "100%",
-        outerWidth : "100%",
-        color : "rgb(144, 202, 249) !important"
-      }
-    
+        innerWidth: "100%",
+        outerWidth: "100%",
+        color: "rgb(144, 202, 249) !important"
+    }
+
 
 });
 
@@ -43,18 +43,20 @@ export default function App() {
     const [searchedCityName, updateSearchedCityName] = useState("")
 
     return <>
-        <CityContext.Provider value={{ searchedCityName, updateSearchedCityName }}>
-            <WeatherContext.Provider value={{ weatherData, updateWeatherData }}>
-                <Header />
-                <div id="mainContainer">
-                    <Routes>
-                        <Route exact path="/" element={<Navigate replace to={'/search'} />} />
-                        <Route exact path="/search" element={<Form />} />
-                        <Route exact path="/data" element={<MyTimeline />} />
-                    </Routes>
-                </div>
-            </WeatherContext.Provider>
-        </CityContext.Provider>
+        <ThemeProvider theme={theme}>
+            <CityContext.Provider value={{ searchedCityName, updateSearchedCityName }}>
+                <WeatherContext.Provider value={{ weatherData, updateWeatherData }}>
+                    <Header />
+                    <div id="mainContainer">
+                        <Routes>
+                            <Route exact path="/" element={<Navigate replace to={'/search'} />} />
+                            <Route exact path="/search" element={<Form />} />
+                            <Route exact path="/data" element={<MyTimeline />} />
+                        </Routes>
+                    </div>
+                </WeatherContext.Provider>
+            </CityContext.Provider>
+        </ThemeProvider>
 
     </>
 
